f9ca332992c8954681a4a2d3093011f21e3c3c03
Paulo Barcelos <paulo@azucrina.org>
2020-05-03 20:37:31 +0200

    first commit

---
+++ cgi-sys/suspendedpage.cgi
@@ -0,0 +1,8 @@
+<HTML>
+<HEAD>
+<META HTTP-EQUIV="refresh" CONTENT="0;URL=http://preludehosting.com/suspenso">
+<title>Conta Suspensa</title>
+</HEAD>
+<BODY>
+</BODY>
+</HTML>
---
+++ newsletter/user/login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<title>azucrina records - subscriber logon</title>
+<link type="text/css" rel="stylesheet" href="/newsletter/themes/shared/css/default.user.css" />
+
+
+
+
+
+</head>
+<body>
+
+<div id="header">
+
+<h1><a href="/">azucrina records</a></h1>
+
+</div>
+
+<div id="content">
+<h2>Subscriber Login</h2>
+
+<p>In order to check your subscription status, update your information, or unsubscribe, you must enter your email address in the field below.</p>
+
+
+
+<form method="post" action="">
+<fieldset>
+<legend>Login</legend>
+
+<div>
+<label for="email"><strong class="required">Your Email:</strong> <span class="error"></span></label>
+<input type="text"name="Email" id="email" value="" size="32" maxlength="60" />
+</div>
+
+</fieldset>
+
+<div class="buttons">
+
+<input type="submit" value="Login" />
+
+</div>
+
+</form>
+
+<br class="clear" />
+
+</div>
+<!-- end content -->
+
+<div id="footer">
+
+<p>- Page fueled by <a href="http://pommo.sourceforge.net/">poMMo</a> mailing management software -</p>
+
+</div>
+<!-- end footer -->
+
+</body>
+</html>
